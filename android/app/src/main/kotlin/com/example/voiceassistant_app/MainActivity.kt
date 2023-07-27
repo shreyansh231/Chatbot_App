81dada61285556d6ae0399664152d9c94493ce02
Shreyansh <shreyanshagrawal1122@gmail.com>
2023-07-27 14:29:04 +0530

    ChatBot App made with Flutter

---
+++ android/app/src/main/kotlin/com/example/voiceassistant_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voiceassistant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,6 @@
+class AssetsManager {
+  static String imagePath = "assets/images";
+  static String userImage = "$imagePath/person.jpg";
+  static String botImage = "$imagePath/chat_logo.jpg";
+  static String openaiLogo = "$imagePath/openai_logo.jpg";
+}
